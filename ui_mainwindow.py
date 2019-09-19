1a415e6d347aaf669cdac0e7aaae46e1748341e1
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-09-19 10:40:14 -0500

    added a GroupBox for Rates

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'mainwindow.ui',
 # licensing of 'mainwindow.ui' applies.
 #
-# Created: Thu Sep 19 10:28:20 2019
+# Created: Thu Sep 19 10:39:38 2019
 #      by: pyside2-uic  running on PySide2 5.12.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,7 +13,7 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 362)
+        MainWindow.resize(800, 428)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
@@ -25,14 +25,19 @@ class Ui_MainWindow(object):
         self.comboBox = QtWidgets.QComboBox(self.groupBox)
         self.comboBox.setObjectName("comboBox")
         self.gridLayout.addWidget(self.comboBox, 0, 0, 1, 1)
-        self.comboBox_2 = QtWidgets.QComboBox(self.groupBox)
-        self.comboBox_2.setObjectName("comboBox_2")
-        self.gridLayout.addWidget(self.comboBox_2, 1, 0, 1, 1)
         self.gridLayout_2.addWidget(self.groupBox, 0, 0, 1, 1)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setEnabled(False)
         self.pushButton.setObjectName("pushButton")
-        self.gridLayout_2.addWidget(self.pushButton, 1, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.pushButton, 2, 0, 1, 1)
+        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.groupBox_2)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.comboBox_2 = QtWidgets.QComboBox(self.groupBox_2)
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.gridLayout_3.addWidget(self.comboBox_2, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.groupBox_2, 1, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 39))
@@ -49,4 +54,5 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "Mic Recorder", None, -1))
         self.groupBox.setTitle(QtWidgets.QApplication.translate("MainWindow", "Interfaces", None, -1))
         self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "Record", None, -1))
+        self.groupBox_2.setTitle(QtWidgets.QApplication.translate("MainWindow", "Rates (Hz)", None, -1))
 
