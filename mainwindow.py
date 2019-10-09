edcf4555bbaf77c6f7f472c7ae6b05c20f8e3af1
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-10-08 22:12:26 -0500

    shows devices and rates

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,8 @@
 from PySide2.QtWidgets import QMainWindow
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
-from recorder import Recorder
+# from recorder import Recorder
+from recorder_pyaudio import Recorder
 
 
 class MainWindow(QMainWindow):
@@ -14,18 +15,22 @@ class MainWindow(QMainWindow):
 
         self.recorder = Recorder()
 
-        for device in self.recorder.devices:
+        for device in self.recorder.devices():
             self.ui.comboBox.addItem(device.deviceName())
 
+
         self.ui.comboBox.currentIndexChanged.connect(self.update_rates)
-        self.ui.comboBox.setCurrentText(self.recorder.default.deviceName())
+        if self.recorder.count():
+            self.ui.comboBox.setCurrentText(self.recorder.default().name)
+            self.update_rates(0)
 
-
-        if self.ui.comboBox.count():
-            self.ui.pushButton.setEnabled(True)
-
-        self.ui.pushButton.clicked.connect(self.record)
-        self.ui.pushButton_2.clicked.connect(self.stop)
+        #
+        #
+        # if self.ui.comboBox.count():
+        #     self.ui.pushButton.setEnabled(True)
+        #
+        # self.ui.pushButton.clicked.connect(self.record)
+        # self.ui.pushButton_2.clicked.connect(self.stop)
 
     def record(self, index):
         self.recorder.record()
@@ -35,6 +40,7 @@ class MainWindow(QMainWindow):
 
     @Slot(int)
     def update_rates(self, index):
+        # pass
         self.ui.comboBox_2.clear()
         for rate in self.recorder.rates(index):
             self.ui.comboBox_2.addItem(str(rate))
--- recorder_pyaudio.py
+++ recorder_pyaudio.py
@@ -1,6 +1,43 @@
+import pyaudio
+
 class Recorder:
+    _devices = []
+
     def __init__(self):
         pass
 
-    def interfaces(self):
-        pass
+    def devices(self):
+        p = pyaudio.PyAudio()
+
+        for i in range(p.get_device_count()):
+            dev = p.get_device_info_by_index(i)
+            # print((i, dev['name'], dev['maxInputChannels']))
+            if dev['maxInputChannels'] >= 1:
+                device = Device()
+                device.name = dev['name']
+                device.rate = dev['defaultSampleRate']
+                device.index = i
+
+                self._devices.append(device)
+
+        return self._devices
+
+    def rates(self, index):
+        return [self._devices[index].rate]
+
+    def count(self):
+        return len(self._devices)
+
+    def default(self):
+        return self._devices[0]
+
+
+class Device:
+    def __init__(self):
+        self.name = ''
+        self.index = -1
+        self.rate = 0
+
+    def deviceName(self):
+        return self.name
+
