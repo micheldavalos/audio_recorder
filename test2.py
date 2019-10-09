530adbcea8fddd74f15460ab1285b37f99c731dd
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-10-08 23:44:59 -0500

    Records audio using a QThread

--- test2.py
+++ test2.py
@@ -1,100 +1,38 @@
+# -*- coding: utf-8 -*-
+'''recorder.py
+Provides WAV recording functionality via two approaches:
+Blocking mode (record for a set duration):
+>>> rec = Recorder(channels=2)
+>>> with rec.open('blocking.wav', 'wb') as recfile:
+...     recfile.record(duration=5.0)
+Non-blocking mode (start and stop recording):
+>>> rec = Recorder(channels=2)
+>>> with rec.open('nonblocking.wav', 'wb') as recfile2:
+...     recfile2.start_recording()
+...     time.sleep(5.0)
+...     recfile2.stop_recording()
+'''
 import pyaudio
 import wave
-from PySide2.QtCore import QThread
-
-class RecorderPyAudio(QThread):
-    _devices = []
-    _rec = None
-    _recfile = None
-    _recording = False
-    _index = 0
-
-    def __init__(self):
-        super(RecorderPyAudio, self).__init__()
-
-    def devices(self):
-        p = pyaudio.PyAudio()
-
-        for i in range(p.get_device_count()):
-            dev = p.get_device_info_by_index(i)
-            # print((i, dev['name'], dev['maxInputChannels']))
-            if dev['maxInputChannels'] >= 1:
-                device = Device()
-                device.name = dev['name']
-                device.rate = dev['defaultSampleRate']
-                device.index = i
-
-                self._devices.append(device)
-
-        return self._devices
-
-    def rates(self, index):
-        return [self._devices[index].rate]
-
-    def count(self):
-        return len(self._devices)
-
-    def default(self):
-        return self._devices[0]
-
-    def start_recording(self, index):
-        self._index = index
-
-        self.start()
-
-        # self._rec = Recorder(channels=2, rate=int(device.rate), index=device.index)
-        #
-        # with self._rec.open('output.wav', 'wb') as self._recfile:
-        #     self._recfile.start_recording()
-
-    def stop_recording(self):
-        self._recording = False
-
-    def run(self):
-        device = self._devices[self._index]
-
-        self._rec = Recorder(channels=2, rate=int(device.rate), index=device.index)
-
-        with self._rec.open('output.wav', 'wb') as recfile:
-            recfile.start_recording()
-
-            self._recording = True
-
-            while self._recording:
-                self.msleep(1)
-                # print('a')
-
-            recfile.stop_recording()
-
-
-class Device:
-    def __init__(self):
-        self.name = ''
-        self.index = -1
-        self.rate = 0
-
-    def deviceName(self):
-        return self.name
-
+import time
 
 class Recorder(object):
     '''A recorder class for recording audio to a WAV file.
     Records in mono by default.
     '''
 
-    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024, index=0):
+    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024):
         self.channels = channels
         self.rate = rate
         self.frames_per_buffer = frames_per_buffer
-        self.index = index
 
     def open(self, fname, mode='wb'):
         return RecordingFile(fname, mode, self.channels, self.rate,
-                            self.frames_per_buffer, self.index)
+                            self.frames_per_buffer)
 
 class RecordingFile(object):
     def __init__(self, fname, mode, channels,
-                rate, frames_per_buffer, index):
+                rate, frames_per_buffer):
         self.fname = fname
         self.mode = mode
         self.channels = channels
@@ -103,7 +41,6 @@ class RecordingFile(object):
         self._pa = pyaudio.PyAudio()
         self.wavefile = self._prepare_file(self.fname, self.mode)
         self._stream = None
-        self._index = index
 
     def __enter__(self):
         return self
@@ -130,8 +67,7 @@ class RecordingFile(object):
                                         rate=self.rate,
                                         input=True,
                                         frames_per_buffer=self.frames_per_buffer,
-                                        stream_callback=self.get_callback(),
-                                     input_device_index=self._index)
+                                        stream_callback=self.get_callback())
         self._stream.start_stream()
         return self
 
@@ -159,3 +95,8 @@ class RecordingFile(object):
         wavefile.setframerate(self.rate)
         return wavefile
 
+rec = Recorder(channels=2)
+with rec.open('nonblocking.wav', 'wb') as recfile2:
+    recfile2.start_recording()
+    time.sleep(5.0)
+    recfile2.stop_recording()
